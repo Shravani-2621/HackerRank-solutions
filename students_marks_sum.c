819873685268841e40c430aaa2c50d81fb4856e1
Shravani-2621 <shravanisurve96@gmail.com>
2026-02-13 19:55:46 +0530

    Create students_marks_sum.c

---
+++ students_marks_sum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  int sum = 0;
+    
+    for(int i = 0; i < number_of_students; i++) {
+        
+        if(gender == 'b' && i % 2 == 0) {
+            sum += marks[i];
+        }
+        else if(gender == 'g' && i % 2 != 0) {
+            sum += marks[i];
+        }
+        
+    }
+    
+    return sum;
+}
+
+int main() {
